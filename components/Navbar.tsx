3021171b236c4fa9627c5f88f4dc798a3f3984fa
Ekim Kael <michelmbili@gmail.com>
2021-07-30 06:43:33 +0100

    Add components and templates directories

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>This is the navbar component</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ templates/auth.template.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthTemplate = ({children}:any) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default AuthTemplate
